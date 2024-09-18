d6d90a63865aebda3e4447cde8bd0f2ba8cea962
behrouzsh <behrouzsharifi7@gmail.com>
2024-09-19 01:49:34 +0330

    url name spaces

--- Django_Basic/home/urls.py
+++ Django_Basic/home/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('detail/<int:todo_id>', views.detail),
+    path('', views.home, name= 'home'),
+    path('detail/<int:todo_id>', views.detail, name= 'detail'),
 ]
 
 
