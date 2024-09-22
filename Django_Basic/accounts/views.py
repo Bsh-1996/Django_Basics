c5d9cd88454efa130d9a93aa9d55423c18ad35f5
behrouzsh <behrouzsharifi7@gmail.com>
2024-09-23 01:57:06 +0330

    user registrations

--- Django_Basic/accounts/views.py
+++ Django_Basic/accounts/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from . forms import UserRegistrationForm
+from django.contrib import messages
 # Create your views here.
 
 
 def user_register(request):
-    return HttpResponse('user regiatration page')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            user.first_name = cd['first_name']
+            user.last_name = cd['last_name']
+            user.save()
+            messages.success(request, 'user registered', 'success')
+            return redirect('home')
+
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form':form})
+
+
+
+
+
+
+
+
